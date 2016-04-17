dbbcad6e672d3298add37e2708dc90eda3c069b1
JohnShuShu <shujohns@gmail.com>
2016-04-17 00:55:59 -0500

    Added CollectData Class

---
+++ src/apps/CollectData.java
@@ -0,0 +1,18 @@
+package apps;
+
+/**
+ * Created by johnshu on 4/14/16.
+ */
+public class CollectData {
+
+    public static void main(String[] args) throws Exception {
+
+//        ItunesStoreScraper.main(args);
+
+        AppStoreScraper.main(args);
+
+        AmazonAppScraper.main(args);
+
+
+    }
+}
